d8d366e01225dd738f7bc962c6590dc5f6da9fdc
ewi josepha <ewijosepha@gmail.com>
2024-03-09 20:51:05 +0100

    info section started

---
+++ app/components/organisms/infoSection.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./styles.css"
+
+function infoSection() {
+
+
+  return (
+    <div>
+      <div className="wrapper">
+        <h1>hello</h1>
+        <div className="innerDiv">
+          <h1>hello</h1>
+        </div>
+        <div className="innerDiv2">
+          <h1>hello</h1>
+        </div>
+       </div>
+    </div>
+  )
+}
+
+export default infoSection
