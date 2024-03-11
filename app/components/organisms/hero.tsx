eff24789390b7a21236e2d898e8e01a9a55d4b4e
nsamedaisy <nsamedaisy@gmail.com>
2024-03-11 13:07:28 +0100

    hero section implementation

--- app/components/organisms/hero.tsx
+++ app/components/organisms/hero.tsx
@@ -1,33 +1,35 @@
-// 'use client'
+'use client'
 
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// const images = [
-//     'image1.jpg',
-//     'image2.jpg',
-//     'image3.jpg',
-//     // Add more image URLs here
-// ];
+const images = [
+    'REImg.webp',
+    'img2.webp',
+    'img1.webp',
+];
 
-// const HeroSection: React.FC = () => {
-//     const [currentImageIndex, setCurrentImageIndex] = useState(0);
+const HeroSection: React.FC = () => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-//     useEffect(() => {
-//         const interval = setInterval(() => {
-//             setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-//         }, 5000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 5000);
 
-//         return () => {
-//             clearInterval(interval);
-//         };
-//     }, []);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
-//     return (
-//         <section className="hero-section">
-//             <img src={images[currentImageIndex]} alt="Hero Image" />
-//             {/* Other content for the hero section */}
-//         </section>
-//     );
-// };
+    return (
+        <section>
+            <img
+                src={images[currentImageIndex]}
+                alt="Hero Image"
+                style={{ width: '100%', height: '800px' }}
+            />
+        </section>
+    );
+};
 
-// export default HeroSection;
+export default HeroSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import InfoSection from "./components/organisms/infoSection";
 import Navbar from "./components/organisms/navbar";
-// import HeroSection from "./components/organisms/hero";
+import HeroSection from "./components/organisms/hero";
 
 export default function Home() {
   return (
     <main>
       <div>
         <Navbar />
-        {/* <HeroSection /> */}
+        <HeroSection />
         {/* <InfoSection /> */}
       </div>
     </main>
