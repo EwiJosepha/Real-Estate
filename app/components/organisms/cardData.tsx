d95bc91640e82fcc323f467915053a4c8a2d754e
nsamedaisy <nsamedaisy@gmail.com>
2024-03-13 22:15:32 +0100

    home page card implementation

--- app/components/organisms/cardData.tsx
+++ app/components/organisms/cardData.tsx
@@ -22,14 +22,22 @@ const properties = [
         ownerName: 'Jane Smith',
         ownerAvatar: 'av2.jpg',
     },
+    {
+        id: 2,
+        image: 'card3.webp',
+        purpose: 'Sell',
+        price: '$500000',
+        ownerName: 'Jane Smith',
+        ownerAvatar: 'img1.webp',
+    },
     // Add more property objects as needed
 ];
 
 const CardData: React.FC = () => {
     return (
         <div className="container mx-auto py-8">
-            <h1 className="text-3xl font-bold mb-4">Latest Properties</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <h1 className="text-3xl font-bold mb-6">Latest Properties</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 {properties.map((property) => (
                     (<Link href={`/property/${property.id}`} key={property.id}>
 
