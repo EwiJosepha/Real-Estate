577420faaac6037fcd6b8cbbe0048a4ec809bde6
nsamedaisy <nsamedaisy@gmail.com>
2024-03-13 22:15:32 +0100

    home page card implementation

--- app/components/organisms/card.tsx
+++ app/components/organisms/card.tsx
@@ -19,15 +19,15 @@ const Card: React.FC<CardProps> = ({
 }) => {
     return (
         <div className="text-base">
-            <div>
-                <img src={image} alt="Property" className="w-full h-48 object-cover mb-4" />
+            <div className='rounded-xl'>
+                <img src={image} alt="Property" className="w-full h- h-72 mb-4" />
                 <p className="text-lg font-bold mb-4">{price}</p>
             </div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between px-8">
                 <p className="text-sm">{ownerName}</p>
                 <img src={ownerAvatar} alt="Owner Avatar" className="w-8 h-8 rounded-full mr-2" />
             </div>
-            <div className='bg-blue py-2 flex text-white justify-between mt-4 px-6 items-center'>
+            <div className='bg-blue py-2 flex text-white justify-between mt-4 px-6 items-center rounded-xl'>
                 <p className=" text-gray-200 mb-2">Apartment/ {purpose}</p>
                 <p>2 rooms, 1 bath</p>
 
