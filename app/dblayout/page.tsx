ad7c7ea1ebaf1072df68f7deef06cdc2823a6d84
nsamedaisy <nsamedaisy@gmail.com>
2024-03-16 00:11:37 +0100

    hashboard layout implementation

--- app/dblayout/page.tsx
+++ app/dblayout/page.tsx
@@ -1,11 +1,17 @@
-'use client'
+"use client";
 
-import React, { ReactNode } from 'react';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
-import { FaArrowAltCircleLeft, FaPerbyte, FaPlus, FaRegHeart, FaRegUser } from 'react-icons/fa';
-import { FaHouseFlag } from 'react-icons/fa6';
-import FooterLogo from '../components/organisms/footerLogo';
+import React, { ReactNode } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import {
+    FaArrowAltCircleLeft,
+    FaPerbyte,
+    FaPlus,
+    FaRegHeart,
+    FaRegUser,
+} from "react-icons/fa";
+import { FaHouseFlag } from "react-icons/fa6";
+import FooterLogo from "../components/organisms/footerLogo";
 
 type LayoutProps = {
     children: ReactNode;
@@ -18,7 +24,8 @@ const DbLayout: React.FC<LayoutProps> = ({ children }) => {
         <div>
             <div className="flex flex-col md:flex-row h-screen bg-blue">
                 {/* Sidebar */}
-                <div className="w-full md:w-1/4 bg-white ">
+                {/* <div className="flex justify-between"> */}
+                <div className="w-1/5 bg-white ">
                     {/* Logo */}
                     <div className="py-2 bg-blue h-36">
                         <FooterLogo />
@@ -62,14 +69,17 @@ const DbLayout: React.FC<LayoutProps> = ({ children }) => {
                         </div>
                     </nav>
                 </div>
-                {/* end of sidebar content  */}
+
+                {/* body Content */}
+                <div className="bg-pink-700 w-full md:w-1/2">{children}</div>
+                {/* </div> */}
 
                 {/* navbar content */}
-                <div className="w-full md:w-3/4 text-white p-6 flex items-center justify-between h-36 py-5">
+                <div className="text-white p-6 flex items-center justify-between h-36 py-5">
+                    <div className="">{/* empty */}</div>
 
                     {/* Desktop Menu */}
                     <div className="hidden md:flex items-center space-x-20">
-
                         <div className="relative group">
                             <button className=" hover:text-gray-300">Add Listings</button>
                         </div>
@@ -85,11 +95,7 @@ const DbLayout: React.FC<LayoutProps> = ({ children }) => {
                     </div>
                 </div>
                 {/* end of navbar content  */}
-
             </div>
-
-            {/* body Content */}
-            {children}
         </div>
     );
 };
