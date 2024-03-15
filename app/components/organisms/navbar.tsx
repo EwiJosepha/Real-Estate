4d5f1b8b924fc0e4f1253f31db07dada8586fc30
nsamedaisy <nsamedaisy@gmail.com>
2024-03-15 11:46:17 +0100

    landing page optimisation

--- app/components/organisms/navbar.tsx
+++ app/components/organisms/navbar.tsx
@@ -19,7 +19,6 @@ const Navbar: React.FC = () => {
                     {/* Logo */}
                     <div className='py-2'>
                         <FooterLogo />
-
                     </div>
 
                     {/* Mobile Menu */}
@@ -71,46 +70,3 @@ const Navbar: React.FC = () => {
 
 export default Navbar;
 
-import React from 'react';
-import Link from 'next/link';
-
-const Navbar: React.FC = () => {
-    return (
-        <nav className="bg-blue max-w-full">
-            <div className="container mx-auto px-4">
-                <div className="flex justify-between items-center h-16 pt-10 pb-10">
-
-                    <div className="flex items-center space-x-20">
-                        <div className="relative">
-                            <Link href="/properties" className="text-white hover:text-gray-300">
-                                Properties
-                            </Link>
-                        </div>
-
-                        <div className="relative group">
-                            <button className="text-white hover:text-gray-300">Other Services</button>
-                            <ul className="absolute hidden group-hover:block bg-gray-800 text-white py-2 mt-2 rounded-md">
-                                <li>
-                                    <Link href="/help" className="block px-4 py-2 hover:bg-gray-700">
-                                        Help
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/contact" className="block px-4 py-2 hover:bg-gray-700">
-                                        Contact
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="relative">
-                            <button className="text-white hover:text-gray-300">Sign In / Sign Up</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
-};
-
-export default Navbar;
