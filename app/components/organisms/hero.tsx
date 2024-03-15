c64f1c9dac179c2cd9bee189947c3e481d9fc1ce
nsamedaisy <nsamedaisy@gmail.com>
2024-03-15 13:43:47 +0100

    landing page optimisation

--- app/components/organisms/hero.tsx
+++ app/components/organisms/hero.tsx
@@ -36,7 +36,7 @@ const HeroSection: React.FC = () => {
             </section>
             <section className="absolute bottom-1/3 transform -translate-x-1/2 left-1/2">
                 <div className="container mx-auto px-4">
-                    <h1 className="text-white font-extrabold md:text-4xl mb-20 sm:text-3xl xs:text-center ">
+                    <h1 className="text-white font-extrabold md:text-4xl mb-20 sm:text-3xl md:text-left xs:text-center">
                         Find Your Dream Home At D&J. The Best Luxury Selection
                     </h1>
 
--- app/components/organisms/infoSection.tsx
+++ app/components/organisms/infoSection.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
-import avatar1 from "../../../public/assets/images/collection_banner_ovf_0.webp";
 import Link from 'next/link';
 import Luxury from './luxury';
 
 function InfoSection() {
   return (
-    <div className="wrapper pt-10 bg-blue text-white">
+    <div className="wrapper pt-14 bg-blue text-white">
       <div className="flex items-center justify-center md:mx-auto md:w-3/4 lg:w-2/3">
-        <div className="innerDiv">
+        <div className="innerDiv text-base w-[80%] mr-20 sm:w-full">
           <div id="line"></div>
           <h1 id="headertext">D&J COLLECTION</h1>
           <p id="descriptionInfo">Since its creation in 2020, D&J Collection Cameroon has consistently maintained its position as the number one specialist real estate company in the luxury segment in Cameroon. With an impressive portfolio of around 1,000 properties and a team of 1,000 certified real estate consultants, it has established itself as an industry leader in the luxury real estate segment. Its unwavering dedication to providing a superior level of service and personalized care sets it apart from its competitors.</p>
@@ -32,9 +31,11 @@ function InfoSection() {
             </div>
           </div>
         </div>
-        <div className="innerDiv2">
-          <Image src={avatar1} alt="avatar" layout="responsive" width={200} height={200} />
+
+        <div className="innerDiv2 hidden md:block">
+          <img id="imagedes" src='banner2.webp' alt="image" style={{ width: '100%', height: '400px', objectFit: 'cover' }} />
         </div>
+
       </div>
       <Luxury />
     </div>
