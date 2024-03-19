0c277cdcaf5209b03447394f505536a6738d8891
nsamedaisy <nsamedaisy@gmail.com>
2024-03-19 17:31:01 +0100

    dashboard and favorite pages implementation

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,7 +11,7 @@ const Dashboard: React.FC = () => {
     return (
         <DdHeaderProvider header="Dashboard" only_header>
             <div className='bg-blue pb-20'>
-                <div className='bg-white py-7 px-8 rounded-lg mb-10'>
+                <div className=' hidden md:block bg-white py-7 px-8 rounded-lg mb-10'>
                     <ul className='flex justify-between items-center'>
                         <li className='text-gray-400'>All Properties <br /><span className='font-serif text-3xl font-bold text-black'>1.9K+</span></li>
                         <li className='text-5xl font-bold'><BsPersonCircle /></li>
@@ -23,14 +23,14 @@ const Dashboard: React.FC = () => {
                         <li className='text-2xl bg-black p-4 rounded-full text-white font-bold'><FaHeart /></li>
                     </ul>
                 </div>
-                <div className='flex'>
+                <div className='md:flex'>
 
-                    <div className='bg-white p-2 rounded-lg font-bold text-xl w-[58%] mr-10'>
+                    <div className='bg-white p-2 rounded-lg font-bold text-xl w-full md:w-[58%] mr-10 mb-10 md:mb-0'>
                         <h1 className='py-4 pl-4'>Property Overview</h1>
                         <BarCharts />
                     </div>
 
-                    <div className='container bg-white rounded-lg w-[38%] py-2 px-4'>
+                    <div className='container bg-white rounded-lg md:w-[38%] py-2 px-4'>
                         <h1 className='text-2xl font-bold font-serif py-4'>Recent Message</h1>
 
                         <div className='border-b-2 border-gray-200 pl-6 space-y-2'>
