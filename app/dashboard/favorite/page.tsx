0c4b2ec6f274e38429bdc522c3ac70402faf9d12
nsamedaisy <nsamedaisy@gmail.com>
2024-03-19 13:14:26 +0100

    favorite page

--- app/dashboard/favorite/page.tsx
+++ app/dashboard/favorite/page.tsx
@@ -1,31 +1,45 @@
 'use client'
 
-import React from "react";
-import { Link } from "react-router-dom";
 import DdHeaderProvider from "@/app/_components/db-header-provider";
 import Card from "@/app/_components/organisms/card";
+import React from "react";
+import Link from "next/link";
 
-interface Property {
-    id: number;
-    image: string;
-    listing: string;
-    price: string;
-    agent: string;
-    avatar: string;
-}
+const properties = [
+    {
+        id: 1,
+        image: 'card4.jpg',
+        listing: 'Apartment/Rent',
+        price: '$35,600',
+        agent: 'Young Filz',
+        avatar: 'av3.jpg',
+    },
+    {
+        id: 2,
+        image: 'card5.jpg',
+        listing: 'House/Sell',
+        price: '$19,000',
+        agent: 'Moses Aloyr',
+        avatar: 'av4.jpg',
+    },
+    {
+        id: 2,
+        image: 'card6.jpg',
+        listing: 'Studio/Sell',
+        price: '$980,000',
+        agent: 'Daisy Smith',
+        avatar: 'av5.jpg',
+    },
+];
 
-interface FavoriteProps {
-    properties: Property[] | undefined;
-}
-
-const Favorite: React.FC<FavoriteProps> = ({ properties }) => {
+const Favorite: React.FC = () => {
     return (
         <DdHeaderProvider header="Favorites" only_header>
-            <div className="container mx-auto py-8 mt-28 items-center justify-center md:mx-auto md:w-3/4 lg:w-2/3">
-                <h1 className="text-3xl font-bold mb-6">Your Favorite Properties</h1>
+            <div className="container mx-auto py-8 mt-28 items-center justify-center lg:w-2/3">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 object-cover">
-                    {properties?.map((property) => (
-                        <Link to={`/property/${property.id}`} key={property.id}>
+                    {properties.map((property) => (
+                        (<Link href={`/property/${property.id}`} key={property.id} legacyBehavior>
+
                             <Card
                                 id={property.id}
                                 image={property.image}
@@ -36,12 +50,12 @@ const Favorite: React.FC<FavoriteProps> = ({ properties }) => {
                                 isFavorite={true}
                                 onToggleFavorite={() => { }}
                             />
-                        </Link>
+
+                        </Link>)
                     ))}
                 </div>
             </div>
         </DdHeaderProvider>
-    );
-};
-
-export default Favorite;
+    )
+}
+export default Favorite
