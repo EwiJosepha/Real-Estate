7ba7699d0d9da8c9189322f9f693534a02a70bba
nsamedaisy <nsamedaisy@gmail.com>
2024-03-21 12:43:37 +0100

    detail page nvabar with responsiveness

--- app/_components/organisms/detailsHero.tsx
+++ app/_components/organisms/detailsHero.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import FooterLogo from './footerLogo'
+import Link from 'next/link'
+import { FaHeart } from 'react-icons/fa'
+import { FaCodeMerge } from 'react-icons/fa6'
 
 function DetailsHero() {
   return (
-    <div>
-      <div className="detailspageWrapper">
+    <>
+      <div className="detailspageWrapper text-black border-b-2 border-gray-400">
+
         <div className="villa">
           <FooterLogo />
+        </div>
 
-          <div className="propertyname">
-            <i className="fa-solid fa-location-dot"></i>
-            <h1 id="villa">Villa Ultra Lucxieu</h1>
-          </div>
-        </div>
         <div className="heart">
-        <i className="fa-regular fa-heart"></i>
-        <i className="fa-solid fa-code-merge"></i>
+          <Link href='/contactPage'> Contact Us</Link>
+          <Link href='/description'>Amenities</Link>
+          <Link href='/#'>Get Help</Link>
+          <FaHeart />
+          <FaCodeMerge />
         </div>
+
       </div>
-    </div>
+    </>
   )
 }
 
