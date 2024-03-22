c5eb82020f55fdc251386b43aaf39bd3e75495c1
ewi josepha <ewijosepha@gmail.com>
2024-03-22 10:16:06 +0100

    mm

--- app/_components/molecules/slider.tsx
+++ app/_components/molecules/slider.tsx
@@ -25,22 +25,6 @@ function SimpleSlider({ images }: Props) {
               height={20} id="sliderId" />
           </div>
         ))}
-
-        {/* <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div> */}
       </Slider>
     </div>
   );
