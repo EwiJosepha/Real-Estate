11e15fbd4bf1e1a6973865f69c5d8762a70b50f9
ewi josepha <ewijosepha@gmail.com>
2024-03-22 17:59:09 +0100

    logging connect to front end

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+"use client"
+
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { FaTimes } from 'react-icons/fa';
+import { NextResponse, } from 'next/server';
+import Cookies from 'js-cookie'
+import { cookies } from 'next/headers';
+
+
 
 const LoginPage: React.FC = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleRegister = async (e: any)=> {
+        e.preventDefault()
+
+        const formData  = {
+            email,
+            hashpassword: password
+        }
+
+        const res = await fetch("http://localhost:4000/auth/signin",{
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+           
+
+            body: JSON.stringify(formData)
+        })
+
+        const res2 =  await res.json()
+        console.log(res2);
+        
+    }
+
+    const token = Cookies.get('next-auth.session-token');
+console.log("token", token)
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-blue">
             <div className="bg-white w-96 p-10 rounded shadow">
@@ -10,12 +48,14 @@ const LoginPage: React.FC = () => {
                     <h2 className="text-2xl font-bold">Login</h2>
                     <Link href='/' passHref ><FaTimes /></Link>
                 </div>
-                <form>
+                <form onSubmit={handleRegister}>
                     <div className="mb-4">
                         <label htmlFor="email" className="block mb-2 font-medium">
                             Email
                         </label>
                         <input
+                        onChange={(e)=> setEmail(e.target.value)}
+
                             type="email"
                             id="email"
                             className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
@@ -26,6 +66,7 @@ const LoginPage: React.FC = () => {
                             Password
                         </label>
                         <input
+                        onChange={(e)=> setEmail(e.target.value)}
                             type="password"
                             id="password"
                             className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
