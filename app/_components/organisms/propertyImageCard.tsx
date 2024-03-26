50d9c787541bef7d8dd500d6c8cad2abe2d0cc47
nsamedaisy <nsamedaisy@gmail.com>
2024-03-26 22:22:24 +0100

    image card optimisation

--- app/_components/organisms/propertyImageCard.tsx
+++ app/_components/organisms/propertyImageCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const PropertyImageCard: React.FC = () => {
@@ -30,34 +31,41 @@ const PropertyImageCard: React.FC = () => {
     return (
         <div className="p-4 shadow shadow-blue rounded-lg">
             <h3 className="text-xl font-semibold mb-4">Photo and Video Upload</h3>
-            {/* <div className="mb-4">
-        <label htmlFor="mainImage" className="block">
-            Main Image
-        </label>
-        <input
-            type="file"
-            id="mainImage"
-            className="w-full"
-        onChange={ }
-        />
-    </div> */}
+
             <div className="mb-4">
-                <label htmlFor="additionalImages" className="block">
-                    Image Attachment*
-                </label>
-                <input
-                    type="file"
-                    id="additionalImages"
-                    className="w-full"
-                    multiple
-                />
+                <div>
+                    <label htmlFor="mainImage" className="block">
+                        Main Image*
+                    </label>
+                    <input
+                        type="file"
+                        id="mainImage"
+                        className="w-full"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="additionalImages" className="block">
+                        Images*
+                    </label>
+                    <input
+                        type="file"
+                        id="additionalImages"
+                        className="w-full grid-cols-4"
+                        multiple
+                    />
+                </div>
             </div>
             {/* field for uplaoded images */}
             <div className="mt-6">
                 <h3 className="text-xl font-semibold mb-4">Uploaded Images</h3>
+                {/* main image  */}
+                <div>
+                    <Image src="" alt="main-img" />
+                </div>
+                {/* images  */}
                 <div className="grid grid-cols-3 gap-4">
                     <div>
-                        <img src='' alt="Uploaded" className="w-full h-40 object-cover rounded-lg" />
+                        <Image src='' alt="Uploaded" className="w-full h-40 object-cover rounded-lg" />
                     </div>
                 </div>
             </div>
